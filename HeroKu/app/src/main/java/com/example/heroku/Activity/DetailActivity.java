a2de5a99df9972ab73a3a90c0346b3a765ac165e
Rikky250503 <rikky25@mhs.mdp.ac.id>
2023-06-03 14:31:37 +0700

    update retrieve dan detail

--- HeroKu/app/src/main/java/com/example/heroku/Activity/DetailActivity.java
+++ HeroKu/app/src/main/java/com/example/heroku/Activity/DetailActivity.java
@@ -2,6 +2,7 @@ package com.example.heroku.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,14 +12,49 @@ import com.example.heroku.R;
 import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
-//
-//    private List<ModelHero> listhero;
-//
-//    private TextView  tvId, tvNama, tvRole, tvLane, tvTahun_rilis, tvDes_hero, tvSkill1, tvDes1, tvSkill2, tvDes2, tvSkill3, tvDes3;
+
+    private TextView  tvNama, tvRole, tvLane, tvTahun_rilis, tvDes_hero, tvSkill1, tvDes1, tvSkill2, tvDes2, tvSkill3, tvDes3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        
+        tvNama= findViewById(R.id.tv_Nama2);
+        tvRole = findViewById(R.id.tv_Role2);
+        tvLane = findViewById(R.id.tv_Lane2);
+        tvTahun_rilis = findViewById(R.id.tv_tahun_rilis2);
+        tvDes_hero = findViewById(R.id.tv_isiDes);
+        tvSkill1 = findViewById(R.id.tv_namaSkill1);
+        tvDes1 = findViewById(R.id.tv_isiDesSkill1);
+        tvSkill2 = findViewById(R.id.tv_namaSkill2);
+        tvDes2 = findViewById(R.id.tv_isiDesSkill2);
+        tvSkill3 = findViewById(R.id.tv_namaSkill3);
+        tvDes3 = findViewById(R.id.tv_isiDesSkill3);
+
+        Intent intent = getIntent();
+        String nama = intent.getStringExtra("varNama");
+        String role = intent.getStringExtra("varRole");
+        String lane = intent.getStringExtra("varLane");
+        String tahun_rilis =intent.getStringExtra("varTahun_rilis");
+        String des_hero = intent.getStringExtra("varDes_hero");
+        String skill1 = intent.getStringExtra("varSkill1");
+        String des1 = intent.getStringExtra("varDes1");
+        String skill2 = intent.getStringExtra("varSkill2");
+        String des2 = intent.getStringExtra("varDes2");
+        String skill3 = intent.getStringExtra("varSkill3");
+        String des3 = intent.getStringExtra("varDes3");
+
+        tvNama.setText(nama);
+        tvRole.setText(role);
+        tvLane.setText(lane);
+        tvTahun_rilis.setText(tahun_rilis);
+        tvDes_hero.setText(des_hero);
+        tvSkill1.setText(skill1);
+        tvDes1.setText(des1);
+        tvSkill2.setText(skill2);
+        tvDes2.setText(des2);
+        tvSkill3.setText(skill3);
+        tvDes3.setText(des3);
     }
 }
--- HeroKu/app/src/main/java/com/example/heroku/Adapter/AdapterHero.java
+++ HeroKu/app/src/main/java/com/example/heroku/Adapter/AdapterHero.java
@@ -16,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.heroku.API.APIRequestData;
 import com.example.heroku.API.RetroServer;
+import com.example.heroku.Activity.DetailActivity;
 import com.example.heroku.Activity.MainActivity;
 
 import com.example.heroku.Model.ModelHero;
@@ -41,7 +42,7 @@ public class AdapterHero extends RecyclerView.Adapter<AdapterHero.VHHero>  {
 
     @NonNull
     @Override
-    public AdapterHero.VHHero onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public VHHero onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View varView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_hero,parent,false);
         return new VHHero(varView);
     }
@@ -55,13 +56,44 @@ public class AdapterHero extends RecyclerView.Adapter<AdapterHero.VHHero>  {
         holder.tvRole.setText(MH.getRole());
         holder.tvLane.setText(MH.getLane());
         holder.tvTahun_rilis.setText(MH.getTahun_rilis());
-        holder.tvDes_hero.setText(MH.getDes_hero());
-        holder.tvSkill1.setText(MH.getSkill1());
-        holder.tvDes1.setText(MH.getDes1());
-        holder.tvSkill2.setText(MH.getSkill2());
-        holder.tvDes2.setText(MH.getDes2());
-        holder.tvSkill3.setText(MH.getSkill3());
-        holder.tvDes3.setText(MH.getDes3());
+//        holder.tvDes_hero.setText(MH.getDes_hero());
+//        holder.tvSkill1.setText(MH.getSkill1());
+//        holder.tvDes1.setText(MH.getDes1());
+//        holder.tvSkill2.setText(MH.getSkill2());
+//        holder.tvDes2.setText(MH.getDes2());
+//        holder.tvSkill3.setText(MH.getSkill3());
+//        holder.tvDes3.setText(MH.getDes3());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama = MH.getNama();
+                String role = MH.getRole();
+                String lane = MH.getLane();
+                String tahun_rilis = MH.getTahun_rilis();
+                String des_hero = MH.getDes_hero();
+                String skill1 = MH.getSkill1();
+                String des1 = MH.getDes1();
+                String skill2 = MH.getSkill2();
+                String des2 = MH.getDes2();
+                String skill3 = MH.getSkill3();
+                String des3 = MH.getDes3();
+
+                Intent intent  = new Intent(holder.itemView.getContext(), DetailActivity.class);
+                intent.putExtra("varNama",nama);
+                intent.putExtra("varRole",role);
+                intent.putExtra("varLane",lane);
+                intent.putExtra("varTahun_rilis",tahun_rilis);
+                intent.putExtra("varDes_hero",des_hero);
+                intent.putExtra("varSkill1",skill1);
+                intent.putExtra("varDes1",des1);
+                intent.putExtra("varSkill2",skill2);
+                intent.putExtra("varDes2",des2);
+                intent.putExtra("varSkill3",skill3);
+                intent.putExtra("varDes3",des3);
+                holder.itemView.getContext().startActivity(intent);
+            }
+        });
 
     }
 
@@ -83,13 +115,13 @@ public class AdapterHero extends RecyclerView.Adapter<AdapterHero.VHHero>  {
             tvRole = itemView.findViewById(R.id.tv_role1);
             tvLane = itemView.findViewById(R.id.tv_lane1);
             tvTahun_rilis = itemView.findViewById(R.id.tv_tahun_rilis1);
-            tvDes_hero = itemView.findViewById(R.id.tv_isiDes);
-            tvSkill1 = itemView.findViewById(R.id.tv_namaSkill1);
-            tvDes1 = itemView.findViewById(R.id.tv_isiDesSkill1);
-            tvSkill2 = itemView.findViewById(R.id.tv_namaSkill2);
-            tvDes2 = itemView.findViewById(R.id.tv_isiDesSkill2);
-            tvSkill3 = itemView.findViewById(R.id.tv_namaSkill3);
-            tvDes3 = itemView.findViewById(R.id.tv_isiDesSkill3);
+//            tvDes_hero = itemView.findViewById(R.id.tv_isiDes);
+//            tvSkill1 = itemView.findViewById(R.id.tv_namaSkill1);
+//            tvDes1 = itemView.findViewById(R.id.tv_isiDesSkill1);
+//            tvSkill2 = itemView.findViewById(R.id.tv_namaSkill2);
+//            tvDes2 = itemView.findViewById(R.id.tv_isiDesSkill2);
+//            tvSkill3 = itemView.findViewById(R.id.tv_namaSkill3);
+//            tvDes3 = itemView.findViewById(R.id.tv_isiDesSkill3);
 
         }
     }
